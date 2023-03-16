3323d4b80861516bed04b42858e0d134d527ea5b
Hisyams <syamesanusi@gmail.com>
2023-03-16 08:58:35 +0700

    Initial Commit

---
+++ EZV Exam Test App/EZV_Exam_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZV_Exam_Test_AppApp.swift
+//  EZV Exam Test App
+//
+//  Created by Hisyam sanusi on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EZV_Exam_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
